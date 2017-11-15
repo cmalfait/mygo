039d05bd3d31db93205c88dd1a401222e845a72d
Chad Malfait <malfaitc@gmail.com>
2017-11-14 19:28:43 -0700

    initial commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func myshit(murphy int) int {
+	fmt.Printf("you sent us: %d\n", murphy)
+	return murphy + 1
+}
+
+func main() {
+	junk := myshit(5)
+	junk1 := myshit(10)
+	fmt.Printf("junk: %d junk2: %d\n", junk, junk1)
+	fmt.Printf()
+}
